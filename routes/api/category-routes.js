553391736a663ec80e2f7c5aa00cadfec99b348f
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-06 12:48:54 -0700

    implemented GET all, GET by ID (without products) and PUT

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,59 @@
 const router = require("express").Router();
+const utils = require("./utils.js");
 const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 const type = "categories";
 
-router.get("/", (req, res) => {
-  // find all categories
-  const scope = "all";
-  res.json(`${req.method} ${scope} ${type}`);
-  // be sure to include its associated Products
+// find all categories
+router.get("/", async (req, res) => {
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (error) {
+    console.log(`Error when getting Categories: ${error.name}`);
+    res.status(500).json(error);
+  }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
-  const scope = "single by ID";
-  res.json(`${req.method} ${scope} ${type}`);
-  // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id);
+    if (!category)
+      res.status(404).json(`No Category esists with id [${req.params.id}]`);
+    else res.status(200).json(category);
+    // be sure to include its associated Products
+  } catch (error) {
+    console.log(`Error when getting Categories: ${error.name}
+      ${error}`);
+    res.status(500).json(error);
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
   const scope = "create new";
-  res.json(`${req.method} ${scope} ${type}`);
+  //res.json(`${req.method} ${scope} ${type}`);
+  try {
+    const category = await Category.create({
+      category_name: req.body.category_name,
+    });
+    if (!category)
+      throw new error(
+        `Failure when attempting to create Category [${req.body.category_name}]`
+      );
+    else res.status(200).json(category);
+  } catch (error) {
+    const type = "Category";
+    if (!utils.handleKnownErrors(req, res, type, error))
+      //unknown error
+      res
+        .status(500)
+        .json(
+          `Failure when attempting to create ${type} [${req.body.category_name}]: ${error.message} ${error}`
+        );
+  }
 });
 
 router.put("/:id", (req, res) => {
